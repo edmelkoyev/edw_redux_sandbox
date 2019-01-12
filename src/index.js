a87a2e0bcd5a9ee696ef20bfffabb56565d47dfc
Edward Melkoyev <edmelkoyev@wiley.com>
2019-01-12 21:12:20 +0300

    redux:bindActionCreators with renamed actions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
 import { decAction, incAction, rndAction } from './actions';
@@ -6,13 +6,14 @@ import { decAction, incAction, rndAction } from './actions';
 const store = createStore(reducer);
 const { dispatch } = store;
 
-const bindActionCreator = (aCreatorFunction, dispatchFunction) => (...params) => {
-  dispatchFunction(aCreatorFunction(...params));
-};
-
-const incDispatch = bindActionCreator(incAction, dispatch);
-const decDispatch = bindActionCreator(decAction, dispatch);
-const rndDispatch = bindActionCreator(rndAction, dispatch);
+const { incDispatch, decDispatch, rndDispatch } = bindActionCreators(
+  {
+    incDispatch: incAction,
+    decDispatch: decAction,
+    rndDispatch: rndAction,
+  },
+  dispatch,
+);
 
 document.getElementById('inc').addEventListener('click', incDispatch);
 document.getElementById('dec').addEventListener('click', decDispatch);
